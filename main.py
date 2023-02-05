117f5cb458b90ad363d5a66e207d24bd51732ddb
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-02-05 09:26:49 +0700

    Add some dataset that are relatively small in size

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from pimage import copy_move
 
 
-copy_move.detect_and_export("input/cattle_gcs500_copy_rb5.png", "output", verbose=True)
+copy_move.detect_and_export("input/small/cattle_gcs500_copy_rb5.png", "output", verbose=True)
